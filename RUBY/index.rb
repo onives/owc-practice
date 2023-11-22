d89b2f58fe48fe0c5a0aba1467eb788ce83d7285
Olive Nives <olivenives63@gmail.com>
2023-11-22 15:43:11 +0300

    adding new test script

--- RUBY/index.rb
+++ RUBY/index.rb
@@ -1,18 +1,23 @@
+character_name = "Nives"
+character_age = "35"
+
 puts "Hello world"
+puts "My name is #{character_name}"
+puts "I am #{character_age} years old"
 
-for a in 1..10 do
-    puts a
-end
+# for a in 1..10 do
+#     puts a
+# end
 
-a = 77 
-if a <50   
-  puts "Student is fail"   
-elsif a >= 50 && a <= 60   
-  puts "Student gets D grade"   
-elsif a >= 70 && a <= 80   
-  puts "Student gets B grade"   
-elsif a >= 80 && a <= 90   
-  puts "Student gets A grade"    
-elsif a >= 90 && a <= 100   
-  puts "Student gets A+ grade"    
-end  
+# a = 77 
+# if a <50   
+#   puts "Student is fail"   
+# elsif a >= 50 && a <= 60   
+#   puts "Student gets D grade"   
+# elsif a >= 70 && a <= 80   
+#   puts "Student gets B grade"   
+# elsif a >= 80 && a <= 90   
+#   puts "Student gets A grade"    
+# elsif a >= 90 && a <= 100   
+#   puts "Student gets A+ grade"    
+# end  
