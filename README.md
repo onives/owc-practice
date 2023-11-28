4b5b734324e5948d8b234fe1eb99d44f074f7f58
Olive Nives <olivenives63@gmail.com>
2023-11-28 11:50:20 +0300

    adding something

--- README.md
+++ README.md
@@ -14,14 +14,16 @@ Python: v3.11.6
 In RUBY, the following versions are used
 
 Ruby: 2.6.10p210
+
 Gem: 3.0.3.1
+
 RSpec 3.12
   - rspec-core 3.12.2
   - rspec-expectations 3.12.3
   - rspec-mocks 3.12.6
   - rspec-support 3.12.1
 
-Testing
+# Testing
 
 Run JS_TDD tests with npm test. They are written in jest, which is configured in jest.config.js.
 
--- RUBY/spec/is_prime_spec.rb
+++ RUBY/spec/is_prime_spec.rb
@@ -2,53 +2,53 @@ require './lib/is_prime'
 
 describe "# is_prime" do
   it "should return true for no.2" do
-    expect(is_prime(2)).to eql(true)
+    expect(is_prime(2)).to be(true)
   end
   it "should return false for no.4" do
-    expect(is_prime(4)).to eql(false)
+    expect(is_prime(4)).to be(false)
   end
   it "should return false for no.6" do
     expect(is_prime(6)).to eql(false)
   end
   it "should return false for no.8" do
-    expect(is_prime(8)).to eql(false)
+    expect(is_prime(8)).to be(false)
   end
   it "should return true for no.3" do
-    expect(is_prime(3)).to eql(true)
+    expect(is_prime(3)).to be(true)
   end
   it "should return false for no.9" do
-    expect(is_prime(9)).to eql(false)
+    expect(is_prime(9)).to be(false)
   end
   it "should return false for no.15" do
     expect(is_prime(15)).to eql(false)
   end
   it "should return false for no.21" do
-    expect(is_prime(21)).to eql(false)
+    expect(is_prime(21)).to be(false)
   end
   it "should return false for no.25" do
-    expect(is_prime(25)).to eql(false)
+    expect(is_prime(25)).to be(false)
   end
   it "should return false for no.35" do
-    expect(is_prime(35)).to eql(false)
+    expect(is_prime(35)).to be(false)
   end
   it "should return false for no.55" do
-    expect(is_prime(55)).to eql(false)
+    expect(is_prime(55)).to be(false)
   end
   it "should return true for no.5" do
-    expect(is_prime(5)).to eql(true)
+    expect(is_prime(5)).to be(true)
   end
   it "should return true for no.7" do
-    expect(is_prime(7)).to eql(true)
+    expect(is_prime(7)).to be(true)
   end
   it "should return false for negative numbers" do
-    expect(is_prime(-5)).to eql(false)
-    expect(is_prime(-7)).to eql(false)
+    expect(is_prime(-5)).to be(false)
+    expect(is_prime(-7)).to be(false)
   end
   it "should return false for no.1" do
-    expect(is_prime(1)).to eql(false)
+    expect(is_prime(1)).to be(false)
   end
   it "should return false for no.0" do
-    expect(is_prime(0)).to eql(false)
+    expect(is_prime(0)).to be(false)
   end
 end
 
